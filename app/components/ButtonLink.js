52d16721c2e987561d4b4c560ccc5fb041f505dd
Patrick Puritscher <blackout289@gmail.com>
2016-05-01 19:18:27 +0200

    updated eslint

--- app/components/ButtonLink.js
+++ app/components/ButtonLink.js
@@ -1,48 +1,48 @@
 import React, {
-  PropTypes
+  PropTypes,
 } from 'react';
 import Radium from 'radium';
 
 const styles = {
-    button: {
-        flex: 1,
-        lineHeight: 2,
-        color: '#fff',
-        textDecoration: 'none',
-        backgroundColor: '#434343',
-        textAlign: 'center',
-        ':hover': {
-            backgroundColor: '#313131',
-            transitionProperty: 'all',
-            transitionDuration: '.2s',
-            transitionTimingFunction: 'ease-in-out',
-            marginTop: -1
-        }
+  button: {
+    flex: 1,
+    lineHeight: 2,
+    color: '#fff',
+    textDecoration: 'none',
+    backgroundColor: '#434343',
+    textAlign: 'center',
+    ':hover': {
+      backgroundColor: '#313131',
+      transitionProperty: 'all',
+      transitionDuration: '.2s',
+      transitionTimingFunction: 'ease-in-out',
+      marginTop: -1,
     },
-    button__twitter: {
-        backgroundColor: 'rgba(85, 172, 238, 0.83)',
-        ':hover': {
-            backgroundColor: '#292f33',
-            transitionProperty: 'all',
-            transitionDuration: '.2s',
-            transitionTimingFunction: 'ease-in-out',
-            marginTop: -1
-        }
-    }
+  },
+  button__twitter: {
+    backgroundColor: 'rgba(85, 172, 238, 0.83)',
+    ':hover': {
+      backgroundColor: '#292f33',
+      transitionProperty: 'all',
+      transitionDuration: '.2s',
+      transitionTimingFunction: 'ease-in-out',
+      marginTop: -1,
+    },
+  },
 };
 
 const ButtonLink = ({ href, colorScheme = 'default', title }) => {
-    const _title = title ? title : href;
-    if (colorScheme === 'twitter') {
-        return (<a style={Object.assign({}, styles.button, styles.button__twitter)} href={href}>{_title}</a>);
-    }
-    return (<a style={styles.button} href={href}>{_title}</a>);
+  const label = title || href;
+  if (colorScheme === 'twitter') {
+    return (<a style={Object.assign({}, styles.button, styles.button__twitter)} href={href}>{label}</a>);
+  }
+  return (<a style={styles.button} href={href}>{label}</a>);
 };
 
 ButtonLink.propTypes = {
   href: PropTypes.string.isRequired,
   colorScheme: PropTypes.string,
-  title: PropTypes.string
+  title: PropTypes.string,
 };
 
 export default Radium(ButtonLink);
--- app/containers/Root.js
+++ app/containers/Root.js
@@ -1,34 +1,32 @@
 import React from 'react';
 import Card from '../components/Card';
 import Hint from '../components/Hint';
-import {StyleRoot} from 'radium';
+import { StyleRoot } from 'radium';
 
 import 'normalize.css';
 import './Root.css';
 
 const styles = {
-    theatre: {
-        flex: 1,
-        display: 'flex',
-        flexDirection: 'column'
-    },
-    stage: {
-        display: 'flex',
-        flexDirection:'column',
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+  theatre: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  stage: {
+    display: 'flex',
+    flexDirection: 'column',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 };
 
-const Root = () => {
-    return (
-      <StyleRoot style={styles.theatre}>
-        <div style={styles.stage}>
-          <Card/>
-        </div>
-        <Hint/>
-      </StyleRoot>
-    );
-};
+const Root = () => (
+  <StyleRoot style={styles.theatre}>
+    <div style={styles.stage}>
+      <Card />
+    </div>
+    <Hint />
+  </StyleRoot>
+);
 export default Root;
